0d6b850efef72f073b1061bcf03e1c30cfab8f6f
Shivashankar-gitH <shiva1894300@gmail.com>
2020-11-23 17:37:30 +0530

    changes

--- weatherapp/src/components/controller/controller.jsx
+++ weatherapp/src/components/controller/controller.jsx
@@ -46,11 +46,11 @@ class Controller extends Component {
 						title: "",
 						notfound: "Data not found try another country/city",
 					});
-					setTimeout(() => {
-						this.setState({
-							notfound: "",
-						});
-					}, 2000);
+					// setTimeout(() => {
+					// 	this.setState({
+					// 		notfound: "",
+					// 	});
+					// }, 2000);
 				}
 			})
 			.catch((error) => console.log(error));
