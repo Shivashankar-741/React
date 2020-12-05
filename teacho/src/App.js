8c0d63a0c66f24785f2a3b155b6cae4eae51c5cc
Shivashankar-gitH <shiva1894300@gmail.com>
2020-12-05 13:24:38 +0530

    setting-files

--- teacho/src/App.js
+++ teacho/src/App.js
@@ -1,10 +1,9 @@
+import Home from "./Applayout/Home/Home";
+
 function App() {
 	return (
 		<div className="App">
-			<h1>teacho app 🔥</h1>
-			<h3>app one</h3>
-			<h1>hi</h1>
-			<h5>hi there im here</h5>
+			<Home />
 		</div>
 	);
 }
---
+++ teacho/src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Navbar = () => {
+	return (
+		<>
+			<h1>Navbar</h1>
+			<h2>Navigation</h2>
+		</>
+	);
+};
+
+export default Navbar;
