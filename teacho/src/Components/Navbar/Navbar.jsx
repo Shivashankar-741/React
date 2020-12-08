4b68735677f60574b001ff11292c254a9d236657
Shivashankar-gitH <shiva1894300@gmail.com>
2020-12-08 16:17:59 +0530

    navbar-navigation

--- teacho/src/Components/Navbar/Navbar.jsx
+++ teacho/src/Components/Navbar/Navbar.jsx
@@ -1,11 +1,36 @@
 import React from "react";
+import "./Navbar.css";
+import logo from "../../assets/logo/Group 45.svg";
 
 const Navbar = () => {
 	return (
-		<>
-			<h1>Navbar</h1>
-			<h2>Navigation</h2>
-		</>
+		<div className="navbar">
+			<div className="logo">
+				<h1 className="teacho">Teacho</h1>
+				<div className="logoImg">
+					<img src={logo} alt="logo" className="logoImgin" />
+				</div>
+			</div>
+			<ul className="nav-list">
+				<li>
+					<a href=" #">Home</a>
+				</li>
+				<li>
+					<a href=" #">Courses</a>
+				</li>
+				<li>
+					<a href=" #">Pricing</a>
+				</li>
+				<li>
+					<a href=" #">About</a>
+				</li>
+				<li>
+					<a href=" #" className="btn">
+						Get started
+					</a>
+				</li>
+			</ul>
+		</div>
 	);
 };
 
