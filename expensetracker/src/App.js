20f17bb9fb013fbda41fd2b39debd3cae8c8d403
Shivashankar-gitH <shiva1894300@gmail.com>
2020-12-14 18:40:40 +0530

    changes

---
+++ expensetracker/src/App.js
@@ -0,0 +1,10 @@
+import ExpenseTracker from "../src/AppLayout/ExpenseTracker/ExpenseTracker.jsx";
+function App() {
+	return (
+		<div className="App">
+			<ExpenseTracker />
+		</div>
+	);
+}
+
+export default App;
